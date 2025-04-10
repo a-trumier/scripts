e6f21b7f0e07e961f898f13f16e0d21a5dbe7e47
Alex Trumier <adtrumier@gmail.com>
2025-04-10 17:26:29 -0600

    added renaming script and fixed other

--- batch_chmod.sh
+++ batch_chmod.sh
@@ -6,15 +6,23 @@
 
 if [ $(($#)) -ne 3 ]; then
     echo "Usage: batch_chmod <directory> <suffix> <new mode>"
+    echo ${1:0:1}
     exit 1
 fi
 
-STUFF=`ls $1 -pc -1`
+if [ ${1:0:1} != "/" ] | [ ${1:0:1} != "~" ]; then
+    PATH_TO_FILES=$PWD/$1
+else
+    PATH_TO_FILES=$1
+fi
+
+STUFF=`ls $PATH_TO_FILES -pc -1`
 SIZE_OF_SUFF=${#2}
 
 for x in $STUFF; do
     SUFF=`echo $x | tail -c $((SIZE_OF_SUFF + 1))`
     if [ $SUFF == $2 ]; then
-        echo chmod $3 $x
+        echo Changing $x modstring to $3
+        chmod $3 $x
     fi
 done
---
+++ scumbag_destroyer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Script that changes the name of every file in a directory with spaces to the
+# same name with underscores.
+# Alex Trumier <adtrumier@gmail.com>
+
+if [ $(($#)) -ne 1 ]; then
+    echo "Usage: scumbag_destroyer.sh <directory>"
+    exit 1
+fi
+
+if [ ${1:0:1} != "/" ] | [ ${1:0:1} != "~" ]; then
+    PATH_TO_FILES=$PWD/$1
+else
+    PATH_TO_FILES=$1
+fi
+
+FILES=`ls $PATH_TO_FILES -pc -1`
+IFS=$'\n'
+for x in $FILES; do
+    NAME=`echo $x | tr -s " " "_"`
+    if [ $x != $NAME ]; then
+        echo Renaming $x to $NAME
+        mv $PATH_TO_FILES/$x $PATH_TO_FILES/$NAME
+    fi
+done
+    
